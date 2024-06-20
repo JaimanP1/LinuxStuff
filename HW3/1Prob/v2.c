0d3b02cc7ee7d1f5b50f265aea772f41cdf04fc6
Jaiman, WSL <jaimanparekh0@gmail.com>
2024-06-19 23:43:51 -0400

    prob1

--- HW3/1Prob/v2.c
+++ HW3/1Prob/v2.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function to read matrix dimension from a binary file
-int readMatrixDimension(const char *filename) {
+int Dimension(const char *filename, int *dim) {
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
-        printf("Error opening file %s\n", filename);
-        exit(1);
+        printf("Could not open file %s\n", filename);
+        return 0;
     }
 
-    int dim;
-    fread(&dim, sizeof(int), 1, file); // Read the dimension from file
+    fread(dim, sizeof(int), 1, file); 
 
     fclose(file);
 
-    if (dim > 100) {
-        printf("Matrix dimension from file '%s' is too large (%d). Maximum allowed is 100.\n", filename, dim);
-        exit(1);
+    if (*dim > 100) {
+        printf("Matrix dimension in file '%s' exceeds 100\n", filename);
+        return 0;
     }
 
-    return dim;
+    return *dim;
 }
 
 int main() {
     int dim1, dim2;
 
-    // Read dimensions from files
-    dim1 = readMatrixDimension("matrix1.bin");
-    dim2 = readMatrixDimension("matrix2.bin");
+    dim1 = Dimension("matrix1.bin", &dim1);
+    dim2 = Dimension("matrix2.bin", &dim2);
 
-    // Compare dimensions
-    if (dim1 != dim2) {
-        printf("Matrix dimensions do not match\n");
-        exit(1);
+    if (dim1 == 0 || dim2 == 0) {
+        return 0; 
     }
 
-    printf("Matrix dimensions match: %d\n", dim1);
+    if (dim1 == dim2) {
+        printf("Matrix dimensions match: %d\n", dim1);
+    } else {
+        printf("Matrix dimensions do not match\n");
+    }
 
     return 0;
 }
-
