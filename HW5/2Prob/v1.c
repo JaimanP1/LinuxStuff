4ef8e7396709c5f2dd8bd548ef4ea28cd513430b
Jaiman, WSL <jaimanparekh0@gmail.com>
2024-07-03 20:47:06 -0400

    hw5 prob2, should be done

--- HW5/2Prob/v1.c
+++ HW5/2Prob/v1.c
@@ -12,8 +12,27 @@ int main( int argc, char **argv){
 				printf("option: %c\n", opt);
 				break;
 			case 'f':
+				printf("option f value: %s\n", optarg); 
+				break;
+			case 's': 
+				printf("option s value: %s\n", optarg);
+				break;
+			case '?':
+				printf("unkown option: %c\n", optopt);
+				break;
+			case ':': 
+				printf("option %c needs a value\n", optopt);
+				break;
+			default:
+				printf("something went wrong");
+				return 0;
 
-
+		}
+	}
+	if (optind < argc) {
+		printf("Remaining arguments:\n");
+		while (optind < argc) {
+		       	printf("  %s\n", argv[optind++]);
 		}
 	}
 }
